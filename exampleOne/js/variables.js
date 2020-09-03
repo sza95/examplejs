f398bdfb8fb3014441d1bc06a4765b3203d9965f
sza95 <sza95@outlook.com>
2020-09-02 21:15:01 -0500

    add exampleOne folder

---
+++ exampleOne/js/variables.js
@@ -0,0 +1,9 @@
+//Scope
+let nombre = 'Fernando'
+let edad = 35
+let estatura = 1.71
+const fechaEspecial = "20-10-2020"
+const estado_casado = true
+//var 
+
+document.write(`Tu nombres es ${nombre} y tu eded es ${edad} y más`)
